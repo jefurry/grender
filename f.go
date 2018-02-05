7c0edacbd67284e4296cdcb62d8b1dbb32c49934
jefurry <jefurry@qq.com>
2018-02-06 04:50:31 +0800

    fixed RandBytes bugs

--- f.go
+++ f.go
@@ -24,9 +24,11 @@ func EnsurePath(path string, dir bool) error {
 }
 
 func RandByte(n int) []byte {
-	chars := []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_'}
+	chars := []byte{
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_',
+	}
 
 	b := make([]byte, n)
 	for i, l := 0, len(chars); i < n; i++ {
